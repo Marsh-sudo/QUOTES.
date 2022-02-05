ed6b65bda5ac9953d02e41c17fb84d49d4d3d965
Marsh-sudo <kelvinmunyao01@gmail.com>
2022-02-06 01:21:24 +0300

    Add quotes Array

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { Quotes } from './quotes';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +8,16 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'QUOTES.';
+
+    wises:Quotes[] = [
+      {name:'Success usually comes to those who are busy looking for it'},
+      {name:'I never dreamed about success.I worked for it....Estee Lauder'},
+      {name:'Do not let yesterday take up too much of today...Will Rodgers'},
+      {name:'Goal setting is the secret to a compelling future. ...Tony Robbins'},
+      {name:'Either you run the day or the day runs you. ...Thomas Jefferson'},
+      {name:'Setting goals is the first step in turning the invisible to visible. ...Tony Robins'},
+      
+
+    ]
+
 }
