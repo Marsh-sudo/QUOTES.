ddeb1968c1e151d5a5b42904e76de53dcd9fa31f
Marsh-sudo <kelvinmunyao01@gmail.com>
2022-02-06 18:22:20 +0300

    imported components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,13 +7,17 @@ import { AppComponent } from './app.component';
 import { HeaderComponent } from './header/header.component';
 import { FooterComponent } from './footer/footer.component';
 import { FormComponent } from './form/form.component';
+import { QuoteDetailComponent } from './quote-detail/quote-detail.component';
+import { QuoteComponent } from './quote/quote.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     HeaderComponent,
     FooterComponent,
-    FormComponent
+    FormComponent,
+    QuoteDetailComponent,
+    QuoteComponent
   ],
   imports: [
     BrowserModule,
