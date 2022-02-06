31bed7ce2fb7c7c840d47b30cf5077de1aadd4e2
Marsh-sudo <kelvinmunyao01@gmail.com>
2022-02-06 18:20:10 +0300

    Removed array property

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -9,22 +9,7 @@ import { Quotes } from './quotes';
 export class AppComponent {
   title = 'QUOTES.';
 
-    wise:Quotes[] = [
-      {name:'Success usually comes to those who are busy looking for it'},
-      {name:'I never dreamed about success.I worked for it....Estee Lauder'},
-      {name:'Do not let yesterday take up too much of today...Will Rodgers'},
-      {name:'Goal setting is the secret to a compelling future. ...Tony Robbins'},
-      {name:'Either you run the day or the day runs you. ...Thomas Jefferson'},
-      {name:'Setting goals is the first step in turning the invisible to visible. ...Tony Robins'},
-      
-
-    ]
-
-    addNewGoal(quote){
-      let quoteLength = this.wise.length;
-      quote.id = quoteLength+1;
-      this.wise.push(quote)
-    }
+    
 
 }
 
